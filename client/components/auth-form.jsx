cfecfed7f806cbbfa70fd3b9006b240c1a9499eb
Rosemary Liu <rliu18@gmail.com>
2022-05-08 23:50:52 +0000

    Preliminary set up of Auth page. Set up dynamic header, redirect, and imported AuthForm component.

---
+++ client/components/auth-form.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class AuthForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <form action="">
+        <div>FORM</div>
+
+      </form>
+    );
+  }
+}
--- client/pages/auth.jsx
+++ client/pages/auth.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import AppContext from '../lib/app-context';
+import AuthForm from '../components/auth-form';
+import Redirect from '../components/redirect';
 
 export default class AuthPage extends React.Component {
   render() {
-    // const { user, route, handleSignIn } = this.context;
+    const { user, route, handleSignIn } = this.context;
+
+    const header = route.path === 'sign-in'
+      ? 'Sign In'
+      : 'Sign Up';
+
+    if (user) return <Redirect to="" />;
 
     return (
-      <div>Hi</div>
+      <div className='container bg-secondary border-radius-10px'>
+        <div className='d-flex flex-column align-items-center'>
+          <header className='text-white fw-bold fs-2'>{header}</header>
+          <AuthForm
+          key={route.path}
+          action={route.path}
+          onSignIn={handleSignIn}
+          />
+        </div>
+      </div>
     );
   }
 }
