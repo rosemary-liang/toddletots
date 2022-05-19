2b120fcb426a2a5404dde07e5a1e6c2d0682a846
Rosemary Liu <rliu18@gmail.com>
2022-05-19 19:31:50 +0000

    Updated Delete Modal message to inform related bookmarks will be deleted

--- client/components/delete-modal.jsx
+++ client/components/delete-modal.jsx
@@ -30,10 +30,11 @@ export default function DeleteModal(props) {
 
       <Modal show={show} onHide={handleClose} dialogClassName='custom-dialog-delete position-absolute'>
         <Modal.Body>
-          <div className='d-flex flex-column fw-bold text-dark-gray text-center p-4'>
+          <div className='d-flex flex-column fw-bold text-dark-gray text-center p-3'>
             {(!loading)
               ? <>
-                  <p className='mb-4'>Are you sure you want to delete this entry?</p>
+                  <p className='mb-1'>Are you sure you want to delete?</p>
+                  <p className='text-danger mb-4 fs-8 fw-lighter text-left'>Related bookmarks will be deleted for all users.</p>
                   <div className='d-flex justify-content-evenly'>
                   <a href="#" onClick={handleDelete}>
                     <button className='yes-button border-radius-10px px-3 px-sm-5 py-1 fw-bold'>yes</button></a>
