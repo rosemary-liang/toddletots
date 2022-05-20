f3284998e986d5f074515eaef9402072a825ec4d
Rosemary Liu <rliu18@gmail.com>
2022-05-20 00:42:24 +0000

    Restored the deleted index.js files containing /lib components

---
+++ client/pages/index.js
@@ -0,0 +1,4 @@
+export { default as parseRoute } from './parse-route';
+export { default as AppContext } from './app-context';
+export { default as decodeToken } from './decode-token';
+export { default as mapStyles } from './map-styles';
